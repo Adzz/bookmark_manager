1f9504fff3ef8877f38ccf78f21db1ceb667a377
Adam Lancaster <lancaster28@hotmail.com>
2015-12-16 12:14:03 +0000

    pushing to heroku

--- models/link.rb
+++ models/link.rb
@@ -12,6 +12,5 @@ class Link
 
 	end
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
-
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize.auto_upgrade!
