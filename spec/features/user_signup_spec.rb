52c240beb8da033205fe14ee52c70373ab325ce5
Adam Lancaster <lancaster28@hotmail.com>
2015-12-27 11:57:35 +0000

    all tests passing

--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -5,18 +5,18 @@ feature 'User sign up' do
     expect(User.first.email).to eq('alice@example.com')
   end
 
-	def sign_up
+	def sign_up(passwordconf: 'oranges!')
 	 	visit '/users/new'
 	  expect(page.status_code).to eq(200)
 	  fill_in :email,    with: 'alice@example.com'
 	  fill_in :password, with: 'oranges!'
-	  fill_in :passwordconf, with: 'oranges!'
+	  fill_in :passwordconf, with: passwordconf
 	  fill_in :name, with: 'Alice'
 	  click_button "Sign up"
 	end
 
 scenario 'with a password that does not match' do
-  expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+  expect { sign_up(passwordconf: 'wrong') }.not_to change(User, :count)
   expect(current_path).to eq('/users/new')
   expect(page).to have_content 'Password and confirmation password do not match'
 end
